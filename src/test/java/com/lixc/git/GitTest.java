954edcb0796cbbccccf21d14132d23ae9fa868c9
Lixc <vc572936348@yeah.net>
2023-07-06 15:17:26 +0800

    First Commit

---
+++ src/test/java/com/lixc/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.lixc.git;
+
+/**
+ * com.lixc.git
+ *
+ * @author Lixc
+ * @version 1.0
+ * @since 2023-07-06 15:15
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Git test");
+    }
+}
